a2511624ec3d6de7e43b59912e40cc4607ba007b
ElTupac <62774311+ElTupac@users.noreply.github.com>
2022-08-03 00:47:54 -0300

    weather api url variable

--- next.config.js
+++ next.config.js
@@ -2,6 +2,7 @@
 const nextConfig = {
   env: {
     WEATHER_API_KEY: process.env.WEATHER_API_KEY,
+    WEATHER_API_URL: process.env.WEATHER_API_URL,
   },
   reactStrictMode: true,
   swcMinify: true,
