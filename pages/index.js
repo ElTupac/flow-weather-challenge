d02aee1788e34f3b3169e4a84168a96a3705b5ad
ElTupac <62774311+ElTupac@users.noreply.github.com>
2022-08-03 14:12:15 -0300

    Added server side rendering for home page

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,25 @@
+import validIpAddress from "@utils/validators/validIpAddress";
+import getDefaultLocations from "@utils/getters/getDefaultLocations";
+import { Search } from "@models/Search";
 import HomeWrapper from "@wrappers/HomeWrapper";
+
+export async function getServerSideProps({ req }) {
+  const props = {};
+
+  const ip = req.headers["x-forwarded-for"] || req.connection.remoteAddress;
+  if (validIpAddress) {
+    const searchByIp = new Search(ip);
+    const searchResults = await searchByIp.getResults();
+    props.search = searchResults;
+  } else
+    props.search = {
+      results: getDefaultLocations(),
+    };
+
+  return {
+    props,
+  };
+}
 
 const Home = () => <HomeWrapper />;
 
