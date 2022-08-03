8ca9752be951614277d732ccd682a34dc5ff235b
ElTupac <62774311+ElTupac@users.noreply.github.com>
2022-08-02 21:10:36 -0300

    Aligning sections center

--- src/components/page-wrappers/HomeWrapper/index.js
+++ src/components/page-wrappers/HomeWrapper/index.js
@@ -2,7 +2,7 @@ import ClimateSelector from "@ui/ClimateSelector";
 import ClimateInfo from "@ui/ClimateInfo";
 
 const HomeWrapper = () => (
-  <div className="container mx-auto md:flex md:gap-x-5">
+  <div className="container mx-auto md:flex items-center md:gap-x-5">
     <ClimateSelector selectedDefault="Cordoba" />
     <ClimateInfo />
   </div>
