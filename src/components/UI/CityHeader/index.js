197e5e220ee31b1825e1c9098e54c9ce2cd11718
ElTupac <62774311+ElTupac@users.noreply.github.com>
2022-08-04 15:14:04 -0300

    Added more enfasis to the header of the page

--- src/components/UI/CityHeader/index.js
+++ src/components/UI/CityHeader/index.js
@@ -2,9 +2,9 @@ import getDateAndTime from "@utils/getters/getDateAndTime";
 
 const CityHeader = ({ cityInfo: { name, region, country, localtime } }) => (
   <div className="flex flex-col items-center justify-center">
-    <div className="w-fit max-w-[50%]">
-      <h1 className="text-xl">{name}</h1>
-      <p className="text-sm leading-none">
+    <div className="w-fit max-w-[60%]">
+      <h1 className="text-2xl leading-tight mb-1">{name}</h1>
+      <p className="leading-none">
         {region}
         {!region ? country : `, ${country}`}
       </p>
